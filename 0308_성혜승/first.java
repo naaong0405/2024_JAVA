67e70b55c9ed80eeb253287930a55f3e322cbe04
Sung Sia <162702280+naaong0405@users.noreply.github.com>
2024-04-19 12:20:22 +0900

    Add files via upload

---
+++ 0308_성혜승/first.java
@@ -0,0 +1,14 @@
+/*
+ *작성일 : 2024년 3월 8일
+ *작성자 : 컴퓨터교육과 202227008 성시아
+ *설명 : 첫 번째 자바 프로그램 
+ */
+
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요.");
+		System.out.print("성시아입니다.");
+		System.out.println("반갑습니다." + "첫 번째 자바 프로그램입니다.");
+	}
+}
